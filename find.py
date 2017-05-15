dff73a9c9b5449b60a80f34e85be7fe160380d0d
Heinz Wüthrich <Heinz-MacBook@Heinzs-MacBook-Pro.local>
2017-05-15 21:54:58 +0200

    neu find

--- find.py
+++ find.py
@@ -1,30 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import time
-'''
-today = time.strftime('%d.%m.%Y')
+import time           
 
-with open('/Users/Heinz-MacBook/Documents/workspace/Privat/testlog.txt') as f:
-    for line in f:
-        if "The GROUP01 are Completet for Run K31T" in line:
-            print line
-            print line[0:10]
-            if line[0:10] == today:
-                print 'super'
-            else:
-                print 'scheisse'
-'''                
+                
 def findGroupsinLog(input, group, run):
     today = time.strftime('%d.%m.%Y')
     with open(input) as f:
         for line in f:
             if "The " + group + " are Completet for Run " + run in line:
-                print line
-                print line[0:10]
+#                print line
+#                print line[0:10]
                 if line[0:10] == today:
-                    print 'super'
+                    print 'super, das File für ' + group + ' ist vorhanden' 
+                    return True
                 else:
-                    print 'scheisse'
-                    
-findGroupsinLog('/Users/Heinz-MacBook/Documents/workspace/Privat/testlog.txt', 'GROUP01', 'K31T')
+                    print 'scheisse kein File vorhanden'
+        return False
+
+
+while not findGroupsinLog('/Users/Heinz-MacBook/Documents/workspace/Privat/testlog.txt', 'GROUP01', 'K31T'):
+    print "Sleeping for 30 sec"
+    time.sleep(5)
+
+
+while not findGroupsinLog('/Users/Heinz-MacBook/Documents/workspace/Privat/testlog.txt', 'GROUP02', 'K31T'):
+    print "Sleeping for 30 sec"
+    time.sleep(5)
+
+
+while not findGroupsinLog('/Users/Heinz-MacBook/Documents/workspace/Privat/testlog.txt', 'GROUP03', 'K31T'):
+    print "Sleeping for 30 sec"
+    time.sleep(5)
     
