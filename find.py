7b3026bcc48b2f0a71e8d30d0fbdef0b9528d357
Heinz Wüthrich <Heinz-MacBook@Heinzs-MacBook-Pro.local>
2017-05-15 20:18:32 +0200

    neu find

--- find.py
+++ find.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import time
+'''
+today = time.strftime('%d.%m.%Y')
 
-import Tkinter
-import _tkinter 
-
-from tkinter import *
-
-    master = Tk()
-    Label(master, text="First Name").grid(row=0)
-    Label(master, text="Last Name").grid(row=1)
-
-    e1 = Entry(master)
-    e2 = Entry(master)
-
-    e1.grid(row=0, column=1)
-    e2.grid(row=1, column=1)
-
-mainloop( )
+with open('/Users/Heinz-MacBook/Documents/workspace/Privat/testlog.txt') as f:
+    for line in f:
+        if "The GROUP01 are Completet for Run K31T" in line:
+            print line
+            print line[0:10]
+            if line[0:10] == today:
+                print 'super'
+            else:
+                print 'scheisse'
+'''                
+def findGroupsinLog(input, group, run):
+    today = time.strftime('%d.%m.%Y')
+    with open(input) as f:
+        for line in f:
+            if "The " + group + " are Completet for Run " + run in line:
+                print line
+                print line[0:10]
+                if line[0:10] == today:
+                    print 'super'
+                else:
+                    print 'scheisse'
+                    
+findGroupsinLog('/Users/Heinz-MacBook/Documents/workspace/Privat/testlog.txt', 'GROUP01', 'K31T')
+    
