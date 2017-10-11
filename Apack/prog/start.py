d0fdbe8cae8b9e7cb60ffcb8396c06bd8d712e2e
Heinz  Wüthrich <heinz.wuethrich1@gmail.com>
2017-10-11 21:09:18 +0200

    Apack

--- Apack/prog/start.py
+++ Apack/prog/start.py
@@ -10,6 +10,10 @@ Created on 11.10.2017
 from Apack.bin.FileChecker import FileExists
 from Apack.bin.FileChecker import IsFile
 
-#FileExists('/Users/Heinz-MacBook/Documents/workspace/gitwue/wuepython/def.py')
+FileExists('/Users/Heinz-MacBook/Documents/workspace/gitwue/wuepython/def.py')
 
 IsFile('/Users/Heinz-MacBook/Documents/workspace/gitwue/wuepython/def.py')
+
+
+if __name__ == "__main__":
+    pass
