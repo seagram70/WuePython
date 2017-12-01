4fdc63cd8e8c079897ec5ad9bb5724e8fb87ee93
Heinz  Wüthrich <heinz.wuethrich1@gmail.com>
2017-12-01 18:41:29 +0100

    neue scripts

--- A/b.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-
-import subprocess
-
-#subprocess.call('a.py')
-#subprocess.Popen( ['/usr/bin/python', '/a.py'], stdout=subprocess.PIPE )
-subprocess.call( ['python', 'a.py'])
-print ('ich bin fertig mit b')
